130a20dbd1660af1938151507c91438740d58ede
Daniel Pouzemski <daniel@podio.com>
2014-10-31 15:02:16 +0100

    Some more handling for server-side auth.

--- test/platform.spec.js
+++ test/platform.spec.js
@@ -75,19 +75,20 @@ describe('PlatformJS', function() {
     it('should return the correct authorization URL for the client auth', function() {
       var redirectURL = 'https://www.myapp.com/oauth';
       var host = {
-        apiURL: 'https://api.podio.com:443',
+        utils: { _getDomain: sinon.stub().returns('podio.com') },
         authType: 'client',
         clientId: 123
       };
       var expectedURL = 'https://podio.com/oauth/authorize?client_id=123&redirect_uri=https%3A%2F%2Fwww.myapp.com%2Foauth&response_type=token';
 
       expect(PlatformJS.prototype.getAuthorizationURL.call(host, redirectURL)).toEqual(expectedURL);
+      expect(host.utils._getDomain.calledOnce).toBe(true);
     });
 
     it('should return the correct authorization URL for the server auth', function() {
       var redirectURL = 'https://www.myapp.com/oauth';
       var host = {
-        apiURL: 'https://api.podio.com:443',
+        utils: { _getDomain: sinon.stub().returns('podio.com') },
         authType: 'server',
         clientId: 123
       };
@@ -99,7 +100,7 @@ describe('PlatformJS', function() {
     it('should throw an error when retrieving an auth URL for password auth', function() {
       var redirectURL = 'https://www.myapp.com/oauth';
       var host = {
-        apiURL: 'https://api.podio.com:443',
+        utils: { _getDomain: sinon.stub().returns('podio.com') },
         authType: 'password',
         clientId: 123
       };
@@ -126,6 +127,150 @@ describe('PlatformJS', function() {
       var host = {};
 
       expect(PlatformJS.prototype.isAuthenticated.call(host)).toBe(false);
+    });
+
+  });
+  
+  describe('getAccessToken', function() {
+    
+    it('should call _authenticate with the right responseData', function() {
+      var host = {
+        authType: 'server',
+        _authenticate: sinon.stub(),
+        _onAccessTokenAcquired: function() {}
+      };
+      var authCode = 'e123';
+      var redirectURL = 'https://www.myapp.com/oauth';
+      var expectedResponseData = {
+        grant_type: 'authorization_code',
+        code: authCode,
+        redirect_url: redirectURL
+      };
+
+      PlatformJS.prototype.getAccessToken.apply(host, [authCode, redirectURL]);
+      expect(host._authenticate.calledOnce).toBe(true);
+      expect(host._authenticate.getCall(0).args[0]).toEqual(expectedResponseData);
+    });
+
+    it('should call the callback and onAccessTokenAcquired when auth request has finished', function() {
+      var responseData = { accessToken: 123 };
+      var host = {
+        authType: 'server',
+        _authenticate: function(requestData, callback) { callback(responseData); },
+        _onAccessTokenAcquired: sinon.stub()
+      };
+      var callback = sinon.stub();
+
+      PlatformJS.prototype.getAccessToken.apply(host, ['e123', 'https://www.myapp.com/oauth', callback]);
+      expect(host._onAccessTokenAcquired.calledOnce).toBe(true);
+      expect(host._onAccessTokenAcquired.calledWithExactly(responseData)).toBe(true);
+      expect(callback.calledOnce).toBe(true);
+    });
+
+    it('should throw an exception if authType is not server', function() {
+      var host = {
+        authType: 'client'
+      };
+
+      expect(PlatformJS.prototype.getAccessToken.bind(host)).toThrow(new Error('In authentication types other than server access token is delivered through a redirect'));
+    });
+    
+  });
+
+  describe('_onAccessTokenAcquired', function() {
+
+    var responseData = {
+      access_token: 'e123',
+      refresh_token: 'a321',
+      expires_in: 4434,
+      ref: {}
+    };
+    var oAuthObject = {
+      accessToken: 'e123',
+      refreshToken: 'a321',
+      expiresIn: 4434,
+      ref: {}
+    };
+
+    it('should set an OAuth object correctly', function() {
+      var host = {};
+
+      PlatformJS.prototype._onAccessTokenAcquired.call(host, responseData);
+
+      expect(host.authObject).toEqual(oAuthObject);
+    });
+
+    it('should save an authObject in the session store', function() {
+      var host = {
+        sessionStore: {
+          set: sinon.stub()
+        },
+        authType: 'client'
+      };
+
+      PlatformJS.prototype._onAccessTokenAcquired.call(host, responseData);
+
+      expect(host.sessionStore.set.calledOnce).toBe(true);
+      expect(host.sessionStore.set.getCall(0).args[0]).toEqual(oAuthObject);
+      expect(host.sessionStore.set.getCall(0).args[1]).toEqual('client');
+    });
+
+  });
+
+  describe('utils._getDomain', function() {
+
+    it('should extract the domain name from a URL', function() {
+      var host = {
+        apiURL: 'https://api.podio.com:443'
+      };
+
+      expect(PlatformJS.prototype.utils._getDomain.call(host)).toEqual('podio.com');
+    });
+
+  });
+
+  describe('_authenticate', function() {
+  
+    it('should construct the request data and url correctly', function() {
+      var host = {
+        utils: { _getDomain: sinon.stub().returns('podio.com') },
+        clientId: 123,
+        clientSecret: 'secret',
+        _authRequest: sinon.stub()
+      };
+      var requestData = { grant_type: 'authorization_code' };
+      var expectedRequestData = {
+        grant_type: 'authorization_code',
+        client_id: 123,
+        client_secret: 'secret'
+      };
+
+      PlatformJS.prototype._authenticate.call(host, requestData);
+
+      expect(host._authRequest.calledOnce).toBe(true);
+      expect(host._authRequest.getCall(0).args[0]).toEqual('https://podio.com/oauth/token');
+      expect(host._authRequest.getCall(0).args[1]).toEqual(expectedRequestData);
+    });
+    
+  });
+
+  describe('_onAuthResponse', function() {
+
+    it('should call the callback with the body if response is ok', function() {
+      var callback = sinon.stub();
+
+      PlatformJS.prototype._onAuthResponse(callback, 'authorization_code', { ok: true, body: 'body' });
+
+      expect(callback.calledOnce).toBe(true);
+      expect(callback.calledWithExactly('body')).toBe(true);
+    });
+
+    it('should raise an exception if authentication failed', function() {
+      var errorMessage = 'Authentication for authorization_code failed';
+
+      expect(function() {
+        PlatformJS.prototype._onAuthResponse(null, 'authorization_code', { ok: false });
+      }).toThrow(new Error(errorMessage));
     });
 
   });
