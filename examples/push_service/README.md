23807a29cd9a829d4b0853ab391d2dd764604b93
Marc Høegh <ebax_90@hotmail.com>
2015-06-26 10:16:47 +0200

    Added push service example

---
+++ examples/push_service/README.md
@@ -0,0 +1,46 @@
+# Podio Push Service Example
+
+A simple example script that demonstrates using podio-js to subscribe to Podio's real-time push service.
+
+It works be creating two instances of the SDK and logging a different user in to each of them.
+User #1 subscribes to push events for new unread notifications and user #2 then assigns a task to user #1, thereby triggering a notification to be sent to user #1.
+
+We verify that we receive this notification and then exists the script.
+
+# Installation
+
+```
+npm install
+```
+
+# Configuration
+First, you will need to create two users in podio and ensure that they have access to the same workspace.
+
+Then place a config file in the root of the ```push_service/``` folder and give it the following contents:
+```
+{
+  "clientId": "[YOUR CLIENT ID]",
+  "clientSecret": "[YOUR CLIENT SECRET]",
+  "user1": {
+    "username": "[USER 1 EMAIL]",
+    "password": "[USER 1 PASSWORD]"
+  },
+  "user2": {
+    "username": "[USER 2 EMAIL]",
+    "password": "[USER 2 PASSWORD]"
+  }
+}
+```
+Replacing the square bracket fields with your relavant information, of course.
+
+# Run
+
+```
+npm start
+```
+
+This script won't start a server, but rather log its output to the console.
+
+# Documentation
+To learn more about Podio's push services and information about how to use different carrier objects to listen for different events, please refer to the following resource:
+- [Working with Podio's push service](https://developers.podio.com/examples/push)
---
+++ examples/push_service/index.js
@@ -0,0 +1,77 @@
+var fs = require('fs');
+var _ = require('lodash');
+var async = require('async');
+var PodioJS = require('../../lib/podio-js');
+
+var config = JSON.parse(fs.readFileSync('./config.json'));
+
+var podio1 = new PodioJS({
+  authType: 'password',
+  clientId: config.clientId,
+  clientSecret: config.clientSecret
+}, {
+    apiURL: config.apiURL,
+});
+
+var podio2 = new PodioJS({
+  authType: 'password',
+  clientId: config.clientId,
+  clientSecret: config.clientSecret
+}, {
+    apiURL: config.apiURL,
+});
+
+function handleError (err) {
+  throw err;
+}
+
+function authUser (connection, userInfo, callback) {
+  connection.authenticateWithCredentials(userInfo.username, userInfo.password, function() {
+    connection.request('get','/user/status')
+    .then(function(responseData) {
+      callback(null, responseData);
+    }).catch(function(err) {
+      callback(err);
+    });
+  })
+}
+
+function onNotificationReceived (notification) {
+  console.log('Notification received!', notification.data);
+  process.exit();
+}
+
+function onTaskCreated (response) {
+  // When the task has been created...
+  console.log('Created task!', response.text);
+  console.log('Now waiting for notification...');
+}
+
+// Log in both users
+async.parallel([
+  _.partial(authUser, podio1, config.user1),
+  _.partial(authUser, podio2, config.user2)
+], function(err, results) {
+
+  if (err) { handleError(err); }
+
+  var user1Data = results[0];
+  
+  // Create the subscription for user 1 to listen for notifications.
+  // More specifically the event type 'notification_unread'
+  podio1.push(user1Data.push).subscribe(onNotificationReceived)
+  .then(function () {
+    
+    console.log('Added subscription');
+    
+    // Have user2 post a task assigned to user1
+    var taskOptions = {
+      responsible: user1Data.profile.user_id,
+      text: 'An example task to generate a notification ' + new Date().toString()
+    };
+
+    podio2.request('post', '/task/', taskOptions)
+    .then(onTaskCreated)
+    .catch(handleError);
+  }).catch(handleError);
+});
